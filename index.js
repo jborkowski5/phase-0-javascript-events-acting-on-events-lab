a5d063e822e1ccc2c4c70883b028a1e3f5e1cc43
Joseph <borkowski.joseph@gmail.com>
2023-07-15 15:03:17 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+ function moveDodgerLeft() {
+    var dodger = document.getElementById("dodger");
+    var leftPosition = parseInt(dodger.style.left) || 0;
+    var newLeftPosition = leftPosition - 5;
+
+    dodger.style.left = newLeftPosition + "px";
+  }
+
+  function moveDodgerRight() {
+    var dodger = document.getElementById("dodger");
+    var leftPosition = parseInt(dodger.style.left) || 0;
+    var newLeftPosition = leftPosition + 5;
+
+    dodger.style.left = newLeftPosition + "px";
+  }
